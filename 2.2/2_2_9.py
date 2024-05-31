d9c325875342a63b2e5291493538fc0c177be7d2
v_rubashko <v_rubashko@wargaming.net>
2024-06-01 00:26:50 +0300

    add 2_2_9.py

--- 2.2/2_2_9.py
+++ 2.2/2_2_9.py
@@ -2,6 +2,7 @@ with open("files.txt", "r", encoding="utf-8") as file:
     files = list(map(str.strip, file.readlines()))
 
 files_dict = {}
+
 for item in files:
     name = item.split()[0]
     size = item.split()[1]
@@ -11,7 +12,26 @@ for item in files:
     files_dict[name.split(".")[1]].sort()
 
 sort_files_dict = dict(sorted(files_dict.items()))
-print(sort_files_dict)
 
+current = {
+    "B": 1,
+    "KB": 1024,
+    "MB": 1048576,
+    "GB": 1073741824
+}
 
-# print(files)
+for value in sort_files_dict.values():
+    size_sum = 0
+    for item in value:
+        size_sum += int(item[1]) * current[item[2]]
+        print(item[0])
+    print("----------")
+    if size_sum / 1073741824 > 1:
+        size_result = str(int(size_sum / 1073741824 + 0.5)) + " GB"
+    elif size_sum / 1048576 > 1:
+        size_result = str(int(size_sum / 1048576 + 0.5)) + " MB"
+    elif size_sum / 1024 > 1:
+        size_result = str(int(size_sum / 1024 + 0.5)) + " KB"
+    else:
+        size_result = str(size_sum) + " B"
+    print(f"Summary: {size_result}\n")
