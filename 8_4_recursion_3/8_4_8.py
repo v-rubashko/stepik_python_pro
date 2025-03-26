d25e25faca85ac1489f50330c9b50c3938b70a3d
v-rubashko <py6axa89@gmail.com>
2025-03-26 21:36:34 +0300

    26.03

--- 8_4_recursion_3/8_4_8.py
+++ 8_4_recursion_3/8_4_8.py
@@ -1,12 +1,15 @@
 def dict_travel(nested_dicts):
-    res = []
-    for key, value in nested_dicts.items():
-        if not isinstance(value, dict):
-            res.append(f'{key}: {value}')
-        if isinstance(value, dict):
-            dict_travel(value)
-    return print(*sorted(res), sep='\n')
+    for k, v in sorted(nested_dicts.items()):
+        if isinstance(v, dict):
+            dict_travel({f'{k}.{key}': val for key, val in v.items()})
+        else:
+            print(f'{k}: {v}')
 
-data = {'a': 1, 'b': {'c': 30, 'a': 10, 'b': 20}, 'c': 2}
+data = {'firstname': 'Тимур',
+        'lastname': 'Гуев',
+        'birthdate': {'day': 10, 'month': 'October', 'year': 1993},
+        'address': {'streetaddress': 'Часовая 25, кв. 127',
+                    'city': {'region': 'Московская область', 'type': 'город', 'cityname': 'Москва'},
+                    'postalcode': '125315'}}
 
 dict_travel(data)
